7a4a6cfd3b310facc442f16331f377dddca3dcea
ryu <rdj94@naver.com>
2018-07-27 00:37:53 +0900

    revise Form

--- winp/GuiForm.py
+++ winp/GuiForm.py
@@ -1,7 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtGui import QIcon, QPixmap
 from mainwindow import Ui_MainWindow
-
+import cv2
+import numpy as np
+import os
 
 class MainWindow(Ui_MainWindow):
     def __init__(self,w):
@@ -17,6 +21,17 @@ class MainWindow(Ui_MainWindow):
     def get_file_name(self):
         filename = QFileDialog.getOpenFileName()
         self.PathText.setText(filename[0])
+        cap = cv2.VideoCapture(filename[0])
+        ret,frame = cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = QtGui.QImage(frame, frame.shape[1], frame.shape[0],frame.strides[0], QtGui.QImage.Format_RGB888)
+        pix = QtGui.QPixmap.fromImage(img)
+        scene = QtWidgets.QGraphicsScene()
+        scene.addPixmap(pix)
+        self.graphicsView.setScene(scene)
+        self.graphicsView.resize(scene.width()/2,scene.height()/2)
+        self.graphicsView.show()
+
 
 if __name__ == "__main__":
     import sys
